877b9f25fbfb61ca6f4cae9530ed36e6c9a2c960
linh <linh1999@hotmail.com>
2022-11-10 15:28:17 +0100

    test commit

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -229,7 +229,7 @@ class ProfCoursTest extends TestCase
             print $record_cours;
         }
         print "################################################################\n\n";
-        $this->assertCount(count(Self::$cours_a), $record_cours_a, "Nombre d'enregistrement égale pour Cours\n");
+        $this->assertCount(count(Self::$cours_a), $record_cours_a, "Nombre d'enregistrement test égale pour Cours\n");
 
         
         /**
